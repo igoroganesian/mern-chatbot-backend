5d2ff052d0123317ca02e1afc151145bd35edd50
Igor Oganesian <igor.oganesian@gmail.com>
2023-10-26 17:43:38 -0400

    const

--- dist/controllers/userControllers.js
+++ dist/controllers/userControllers.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
 import { createToken } from "../utils/tokenManager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
@@ -20,6 +21,23 @@ export const userSignup = async (req, res, next) => {
         const hashedPassword = await hash(password, 12);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
+        //create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/"
+        });
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true
+        });
         return res.status(201).json({ message: "OK", id: user._id.toString() });
     }
     catch (error) {
@@ -38,10 +56,16 @@ export const userLogin = async (req, res, next) => {
         if (!isPasswordCorrect) {
             return res.status(403).send("Incorrect password");
         }
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/"
+        });
         const token = createToken(user._id.toString(), user.email, "7d");
         const expires = new Date();
         expires.setDate(expires.getDate() + 7);
-        res.cookie("auth_token", token, {
+        res.cookie(COOKIE_NAME, token, {
             path: "/",
             domain: "localhost",
             expires,
