ac745fd4519757e60d90d96b2f27a721d04f8697
Igor Oganesian <igor.oganesian@gmail.com>
2023-11-03 19:45:40 -0400

    added cors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import appRouter from './routes/index.js';
+import cors from 'cors';
 config();
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(morgan("dev"));
 
 //middleware
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/api/v1", appRouter);
